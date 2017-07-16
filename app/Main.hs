aaa67d8b81cb0e0483ba8ca1ffb5b6b21a3be285
Kenta Hamanaka <hamanaka@cs.info.mie-u.ac.jp>
2017-07-17 05:53:08 +0900

    Add main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+import LambdaParse
+import LambdaTyping
+import System.Console.Haskeline
 
 main :: IO ()
-main = someFunc
+main = runInputT defaultSettings loop
+
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine "\x03BB> "
+  case minput of
+    Nothing -> return ()
+    Just "quit" -> return ()
+    Just input -> do
+      printeval input
+      loop
+
+printeval :: String -> InputT IO ()
+printeval s = case parse s of
+  Left error -> outputStrLn $ "Parse error: " ++ show error
+  Right exp  -> outputStrLn $ show exp ++ " :: " ++ show (typeof exp)
